9a8b0a8106763b65d7b7294a2dfa2145463a7866
George Gloyens <george.gloyens@phntms.com>
2024-02-02 09:27:46 +0000

    Initial commit

---
+++ src/app/styles.ts
@@ -0,0 +1,15 @@
+import { styled } from "@phntms/css-components";
+
+import css from "./page.module.scss";
+
+export const PageContainer = styled("main", {
+  css: css.PageContainer,
+});
+
+export const Column = styled("div", {
+  css: css.Column,
+});
+
+export const ComponentList = styled("ul", {
+  css: css.ComponentList,
+});
---
+++ src/app/styles/fonts.ts
@@ -0,0 +1,10 @@
+import { JetBrains_Mono } from "next/font/google";
+
+export const JB_MONO = JetBrains_Mono({
+  variable: "--jb-mono",
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  fallback: ["sans-serif"],
+});
+
+export const fontClasses = [JB_MONO.variable].join(" ");
