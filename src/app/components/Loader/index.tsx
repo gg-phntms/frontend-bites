79ff6caf9673207b3b90e34977b2367f815c255e
George Gloyens <george.gloyens@phntms.com>
2024-02-07 16:33:23 +0000

    Loader polish

--- src/app/components/Loader/index.tsx
+++ src/app/components/Loader/index.tsx
@@ -36,15 +36,42 @@ const Loader = ({ text }: Props) => {
       tl.current = gsap
         .timeline({ delay: 0.5 })
         .addLabel("start")
-        .fromTo(items, { opacity: 0 }, { opacity: 1, stagger: 0.1 }, "start")
-        .fromTo(odd, { y: 0 }, { y: -40 }, "start")
-        .fromTo(even, { y: 80 }, { y: 40 }, "start")
+        .fromTo(
+          items,
+          { opacity: 0 },
+          { opacity: 1, duration: 1, stagger: 0.4 },
+          "start"
+        )
+        .fromTo(
+          odd,
+          { y: 0 },
+          { y: -40, stagger: 0.8, duration: 1, ease: "power2.out", delay: 0.4 },
+          "start"
+        )
+        .fromTo(
+          even,
+          { y: 80 },
+          { y: 40, stagger: 0.8, duration: 1, ease: "power2.out" },
+          "start"
+        )
         .addLabel("center")
-        .to(odd, { y: 0, delay: 0.2 }, "center")
-        .to(even, { y: 0, delay: 0.2 }, "center")
+        .to(
+          odd,
+          { y: 0, delay: 0.2, duration: 1, ease: "power2.out" },
+          "center"
+        )
+        .to(
+          even,
+          { y: 0, delay: 0.2, duration: 1, ease: "power2.out" },
+          "center"
+        )
         .addLabel("hide")
         .to(container.current, { overflow: "hidden" }, "hide")
-        .to(items, { y: 30, delay: 1, stagger: 0.05 }, "hide")
+        .to(
+          items,
+          { y: 30, delay: 0.5, ease: "power2.in", stagger: 0.2 },
+          "hide"
+        )
         // Remember to delete the button if you're reusing this component
         .addLabel("showButton")
         .to(button, { display: "block" }, "showButton")
