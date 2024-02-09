f506ed61e1b060a66c4c82c69f52bc9fc9bdc5e5
George Gloyens <george.gloyens@phntms.com>
2024-02-09 15:58:32 +0000

    Add range toggles

--- src/app/components/3DBlob/index.tsx
+++ src/app/components/3DBlob/index.tsx
@@ -5,24 +5,32 @@ import { Mesh, Vector3 } from "three";
 import { useMemo, useRef } from "react";
 import { createNoise4D } from "simplex-noise";
 
-const Blob = () => {
+interface Props {
+  frequency?: number;
+  speed: number;
+  amplitude: number;
+}
+
+const Blob = ({ frequency, speed, amplitude }: Props) => {
   const blobRef = useRef<Mesh>(null);
   const materialRef = useRef<THREE.ShaderMaterial>(null);
 
+  const spikes = frequency || 0;
   const noise4D = createNoise4D();
 
   // Geometry
   const vertexShader = `
     uniform float u_intensity;
     uniform float u_time;
+    uniform float u_speed;
     
     varying vec2 vUv;
     varying float vDisplacement;
     
     void main() {
       vUv = uv;
-      vec3 newPosition = position + normal * vec3(u_intensity * sin(position.y * 3.0 + u_time));
-      vDisplacement = sin(position.y * 10.0 + u_time);
+      vec3 newPosition = position + normal * vec3(u_intensity * sin(position.x * 3.0 + u_time * u_speed));
+      vDisplacement = sin(position.x * 1.0 + u_time);
       gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
     }
   `;
@@ -31,13 +39,14 @@ const Blob = () => {
   const fragmentShader = `
     uniform float u_intensity;
     uniform float u_time;
+    uniform float u_speed;
     
     varying vec2 vUv;
     varying float vDisplacement;
     
     void main() {
-      float distort = 2.0 * vDisplacement * u_intensity * sin(vUv.y * 10.0 + u_time);
-      vec3 color = vec3(abs(vUv - 0.5) * 2.0  * (1.0 - distort), 1.0);
+      float distort = 2.0 * vDisplacement * u_intensity * sin(vUv.y * 1.0 + u_time);
+      vec3 color = vec3(abs(vUv - 0.5) * 2.0  * (1.0 - distort), 1);
       gl_FragColor = vec4(color, 1.0);
     }
   `;
@@ -45,18 +54,23 @@ const Blob = () => {
   const uniforms = useMemo(() => {
     return {
       u_time: { value: 0 },
-      u_intensity: { value: 0.2 },
+      u_intensity: { value: 0.1 },
+      u_speed: { value: 1 },
     };
   }, []);
 
   useFrame(({ clock }) => {
     const time = clock.getElapsedTime() * 0.00001;
+
     if (materialRef.current) {
       materialRef.current.uniforms.u_time.value = clock.elapsedTime;
+      materialRef.current.uniforms.u_speed.value = speed;
     }
 
     if (blobRef.current) {
       const vertices = blobRef.current.geometry.attributes.position;
+      blobRef.current.rotation.x += 0.001 * speed;
+      blobRef.current.rotation.z += 0.001 * speed;
 
       const updatedVertices = Array.from({ length: vertices.count }, (_, i) => {
         const vertex = new Vector3(
@@ -68,7 +82,16 @@ const Blob = () => {
         vertex
           .normalize()
           .multiplyScalar(
-            1 + 0.3 * noise4D(vertex.x, vertex.y, vertex.z * time, 3)
+            1.5 +
+              amplitude *
+                (0.5 +
+                  Math.sin(clock.getElapsedTime()) *
+                    noise4D(
+                      vertex.x * spikes,
+                      vertex.y * spikes,
+                      vertex.z * spikes + time,
+                      4
+                    ))
           );
 
         return [vertex.x, vertex.y, vertex.z];
@@ -82,7 +105,7 @@ const Blob = () => {
 
   return (
     <mesh ref={blobRef}>
-      <sphereGeometry args={[2, 32, 32]} />
+      <icosahedronGeometry args={[2, 32]} />
       <shaderMaterial
         ref={materialRef}
         fragmentShader={fragmentShader}
