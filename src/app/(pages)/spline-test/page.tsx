e03f3f98128b93af4e5da7971f3b3e6aa89dadc5
George Gloyens <george.gloyens@phntms.com>
2024-10-21 10:54:49 +0100

    Add Spline test

---
+++ src/app/(pages)/spline-test/page.tsx
@@ -0,0 +1,16 @@
+import Header from "@/app/components/Header";
+import Spline from '@splinetool/react-spline/next';
+import { PageContainer, Column } from "@/app/styles/styles";
+
+export default function Page() {
+  return (
+    <PageContainer>
+      <Header text="Spline Test" />
+      <Column>
+        <p>Hosted on Spline's CDN:</p>
+        <Spline scene="https://prod.spline.design/KFonZGtsoUXP-qx7/scene.splinecode" />
+        <p>[TODO] Hosted locally:</p>
+      </Column>
+    </PageContainer>
+  );
+}
